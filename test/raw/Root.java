14691a8d8535aa50eaeb9383ee9e699b33d828cd
Andre Vanha <andre@askitservices.com>
2014-11-10 21:12:27 -0700

    UTF-8 test, removed raw (un-preprocessed) javapp source file.

--- test/raw/Root.java
+++ test/raw/Root.java
@@ -1,4 +1,6 @@
 public class Root {
+	// Comment with some Chinese characters: 广州
+
 	public void run() {
 #ifdef RELEASE
 		validate();
@@ -10,4 +12,9 @@ public class Root {
 	public void test() {
 	}
 #endif
+
+#ifndef RELEASE
+	public void internal() {
+	}
+#endif 
 }
